559ed002d79a24fdc5cde080a31abe256a968915
CuiXY <100781690+xyangcui@users.noreply.github.com>
2023-12-03 09:38:37 +0800

    Add files via upload

---
+++ Master-script-backup/Master-script-backup-20231203/script/replot.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+
+export wkdir1="/home/sunming/data5/cuixy/global_jets"
+
+# box1
+#bash ${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 10 10 20 20 box1 100 20
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 10 10 20 20 box1 100 20"
+#box2
+#bash ${wkdir1}/script/calc_plot_backup.sh EAJfil30-60 15 15 30 30 box2 60 30
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJfil30-60 15 15 30 30 box2 60 30"
+
+#bash ${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 15 15 30 30 box2 100 20
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 15 15 30 30 box2 100 20"
+#box3
+#bash ${wkdir1}/script/calc_plot_backup.sh EAJfil30-60 20 20 35 35 box3 60 30
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJfil30-60 20 20 35 35 box3 60 30"
+
+#bash ${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 20 20 35 35 box3 100 20
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 20 20 35 35 box3 100 20"
+
+#box4
+#bash ${wkdir1}/script/calc_plot_backup.sh EAJfil30-60 25 25 40 40 box4 60 30
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJfil30-60 25 25 40 40 box4 60 30"
+
+#bash ${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 25 25 40 40 box4 100 20
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJfil20-100 25 25 40 40 box4 100 20"
+
+### non-filter.
+# box1
+#bash ${wkdir1}/script/calc_plot_backup_deseason.sh EAJ 10 10 20 20 box1 100 20
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJ 10 10 20 20 box1 100 20"
+
+#box2
+#bash ${wkdir1}/script/calc_plot_backup_deseason.sh EAJ 15 15 30 30 box2 60 30
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJ 15 15 30 30 box2 60 30"
+
+#box3
+#bash ${wkdir1}/script/calc_plot_backup_deseason.sh EAJ 20 20 35 35 box3 60 30
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJ 20 20 35 35 box3 60 30"
+
+#box4
+#bash ${wkdir1}/script/calc_plot_backup_deseason.sh EAJ 25 25 40 40 box4 60 30
+#echo "${wkdir1}/script/calc_plot_backup.sh EAJ 25 25 40 40 box4 60 30"
+
+################################################################################
+# SWJ
+
+#box1
+bash ${wkdir1}/script/calc_plot_backup.sh SWJfil30-60 10 10 20 110 box1 60 30
+echo "${wkdir1}/script/calc_plot_backup.sh SWJfil30-60 10 10 20 110 box1 60 30"
+
+bash ${wkdir1}/script/calc_plot_backup.sh SWJfil20-100 10 10 20 110 box1 100 20
+echo "${wkdir1}/script/calc_plot_backup.sh SWJfil20-100 10 10 20 110 box1 100 20"
+
+#box2
+bash ${wkdir1}/script/calc_plot_backup.sh SWJfil30-60 15 15 46 134 box2 60 30
+echo "${wkdir1}/script/calc_plot_backup.sh SWJfil30-60 15 15 46 134 box2 60 30"
+
+bash ${wkdir1}/script/calc_plot_backup.sh SWJfil20-100 15 15 46 134 box2 100 20
+echo "${wkdir1}/script/calc_plot_backup.sh SWJfil20-100 15 15 46 134 box2 100 20"
+
+
+
+### non-filter.
+# box1
+bash ${wkdir1}/script/calc_plot_backup_deseason.sh SWJ 10 10 20 110 box1 100 20
+echo "${wkdir1}/script/calc_plot_backup_deseason.sh SWJ 10 10 20 110 box1 100 20"
+
+#box2
+bash ${wkdir1}/script/calc_plot_backup_deseason.sh SWJ 15 15 46 134 box2 60 30
+echo "${wkdir1}/script/calc_plot_backup_deseason.sh SWJ 15 15 46 134 box2 60 30"
+
+
+
