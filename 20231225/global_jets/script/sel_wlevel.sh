0106eb1f86627b93c4d41a60560c6fd9b358f4cb
BOBOKA <100781690+xyangcui@users.noreply.github.com>
2024-09-30 12:03:48 +0800

    20240930 backup

--- 20231225/global_jets/script/sel_wlevel.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-export ddir="/home/sunming/data5/cuixy/DATA/ERA-5/daily/wprs/raw_data"
-export odir="/home/sunming/data5/cuixy/global_jets/data"
-
-     cdo -O sellevel,250,300,350,400,450,\
-500 -sellonlatbox,-180,180,-30,90 -mergetime\
- ${ddir}/day_w_flt_??.nc ${odir}/ERA5_daily_mean_wlevel_1979-2022.nc
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # script_backup
 Config files for my GitHub profile.
+
+2024-09-30
+delete backup files in 2023. add MJO-PC scripts and MJO events date and PC.
